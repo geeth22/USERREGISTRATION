06580ac338cf26275d0fa3d4d0cdb50f28da51df
geeth22 <geethchandra22@gmail.com>
2020-11-03 13:00:55 +0530

    Enter valid first name starts with upper case and has minimum 3 characters

--- UserRegistration.java
+++ UserRegistration.java
@@ -1,2 +1,29 @@
-/*  User Registration System needs to ensure all validations are in place during the User Entry  */
+import java.util.Scanner;
+import java.util.regex.Pattern;
 
+public class UserRegistration {
+
+    Scanner scanner = new Scanner(System.in);
+
+    public static final String NAMEPATTERN = "[A-Z][A-Za-z]{2,}";
+
+    public static void main(String[] args) {
+        UserRegistration userRegistration = new UserRegistration();
+        userRegistration.readInput();
+    }
+    private void readInput() {
+        System.out.println("Enter first name ");
+        String firstName = scanner.next();
+        validateInput(NAMEPATTERN,firstName);
+    }
+
+    private void validateInput(String pattern, String input) {
+
+        if(Pattern.matches(pattern, input)) {
+            System.out.println("Valid");
+        }
+        else {
+            System.out.println("Invalid");
+        }
+    }
+}
